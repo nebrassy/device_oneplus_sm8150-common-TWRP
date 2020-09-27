52c48da907314e420988a4834f4b21fabf8a1f89
LuK1337 <priv.luk@gmail.com>
2020-09-27 17:45:33 +0200

    sm8150-common: Use libcutils-v29.so for libdpmframework.so
    * v30 one doesn't have `strdup8to16` symbol in it.
    
    Change-Id: Id2b4bb3017bc24c817dbe4b03834960aa1a4b444

--- extract-files.sh
+++ extract-files.sh
@@ -41,6 +41,12 @@ function blob_fixup() {
     etc/permissions/qti_libpermissions.xml)
         sed -i -e 's|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-java|g' "${2}"
         ;;
+    product/lib/libdpmframework.so)
+        sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
+        ;;
+    product/lib64/libdpmframework.so)
+        sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
+        ;;
     esac
 }
 
