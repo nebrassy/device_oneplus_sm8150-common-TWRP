42f790c215951b65347a4f5f99da1edf260b3eb7
Julian Veit <claymore1298@gmail.com>
2020-09-27 09:39:07 +0200

    sm8150-common: sensors: Use log/log.h instead of cutils/log.h
    Change-Id: I4b68ad9d581b929fb98336c63585205481a8f788

--- sensors/multihal.cpp
+++ sensors/multihal.cpp
@@ -19,7 +19,7 @@
 
 #define LOG_TAG "sensors_multihal"
 #define LOG_NDEBUG 1
-#include <cutils/log.h>
+#include <log/log.h>
 #include <cutils/atomic.h>
 #include <cutils/properties.h>
 #include <hardware/sensors.h>
