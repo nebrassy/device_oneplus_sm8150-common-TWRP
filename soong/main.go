c02b54aaa51d23583db929a8dcd17e93d9d55960
Ralf Luther <luther.ralf@gmail.com>
2021-06-13 16:18:31 +0200

    sm8150-common: sensors: Move to AOSP Soong conditionals
    Original author: "Sebastiano Barezzi <barezzisebastiano@gmail.com>"
    
    Change-Id: I220357b268053463044e2780ae70dfff3bb5a301

--- soong/main.go
+++ soong/main.go
@@ -6,5 +6,4 @@ import (
 
 func init() {
     android.RegisterModuleType("oneplus_msmnile_bootctrl_library", bootctrlLibraryFactory)
-    android.RegisterModuleType("oneplus_msmnile_sensors_hal_binary", sensorsHalBinaryFactory)
 }
